55c8ebd09fba5c14478d448b85d4106c9afcedff
Shubham Hire <shubham.hire2006@gmail.com>
2026-02-23 16:01:31 +0530

    Prepare for Render deployment

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Render build script
+set -o errexit
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+cd studproject
+python manage.py collectstatic --no-input
+python manage.py migrate
--- studproject/studproject/settings.py
+++ studproject/studproject/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=4f1%lsbl7fsh0g%-3j3s3&=30_#+j&bj(t!!vaiol=7-7sa#k'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'django-insecure-=4f1%lsbl7fsh0g%-3j3s3&=30_#+j&bj(t!!vaiol=7-7sa#k'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
@@ -42,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,12 +78,13 @@ WSGI_APPLICATION = 'studproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+# Uses PostgreSQL on Render (via DATABASE_URL env var), SQLite locally
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+    )
 }
 
 
@@ -117,6 +124,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'studapp' / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 # Media files (Uploaded notes)
 MEDIA_URL = '/media/'
